df71f07e89661093b03dbe549b75700205971a89
TXZhe <450111801@qq.com>
2019-03-31 13:31:58 +0800

    add 高精度

---
+++ 0_model.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<cstdio>
+#include<string>
+#include<map>
+#include<queue>
+#include<vector>
+#include<algorithm> 
+
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
